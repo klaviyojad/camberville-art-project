d46a39f192f424ca221e1582e9de44fb91a488c1
klaviyojad <38661954+klaviyojad@users.noreply.github.com>
2021-09-21 19:43:47 +0000

    Deploying to gh-pages from  @ f5b897a708834865cce0af84cb6c55da89f5ad8d 🚀

---
+++ precache-manifest.182b6593759f681de347c5e8a93edb07.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e11e97121cd6ed367b77ed451cdf1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf988673f5dfbc576a39",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "7ef5f1fe0956d1ac527b",
+    "url": "/static/css/main.99b4b6ca.chunk.css"
+  },
+  {
+    "revision": "bf988673f5dfbc576a39",
+    "url": "/static/js/2.3d21c852.chunk.js"
+  },
+  {
+    "revision": "3bce56f6df7cdc700d7e7e337ac342db",
+    "url": "/static/js/2.3d21c852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef5f1fe0956d1ac527b",
+    "url": "/static/js/main.ac7c1167.chunk.js"
+  },
+  {
+    "revision": "b60bf47c74fc4b48dea0",
+    "url": "/static/js/runtime-main.93cafc07.js"
+  },
+  {
+    "revision": "409f08f7691a7370db0841917e1154a8",
+    "url": "/static/media/animals.409f08f7.svg"
+  },
+  {
+    "revision": "3f93822374ba6ad35231f82495fe8683",
+    "url": "/static/media/earth.3f938223.svg"
+  },
+  {
+    "revision": "f09e309aa4e6dfbb9077dae227956bc9",
+    "url": "/static/media/home.f09e309a.svg"
+  },
+  {
+    "revision": "81c82435db8edc6bed1996f30170d6ea",
+    "url": "/static/media/love.81c82435.svg"
+  },
+  {
+    "revision": "eb505191b87a727f05ee9d1310ec23f3",
+    "url": "/static/media/ocean.eb505191.svg"
+  },
+  {
+    "revision": "25382a358fbf2a180e61dfadc21cdcd8",
+    "url": "/static/media/plant.25382a35.svg"
+  },
+  {
+    "revision": "5e892f677ee0dea1536d534d67735dcd",
+    "url": "/static/media/rainbow.5e892f67.svg"
+  },
+  {
+    "revision": "714cf2e08fd531712337d14d97e7a2e3",
+    "url": "/static/media/shapes.714cf2e0.svg"
+  },
+  {
+    "revision": "07427359d1ab332b95c472fb86ed9942",
+    "url": "/static/media/sky.07427359.svg"
+  },
+  {
+    "revision": "af113fcbdf0fc2fd6a27f454e33f6661",
+    "url": "/static/media/spring.af113fcb.svg"
+  },
+  {
+    "revision": "0981839327976182a988ef5502189b1a",
+    "url": "/static/media/thankyou.09818393.svg"
+  }
+]);
